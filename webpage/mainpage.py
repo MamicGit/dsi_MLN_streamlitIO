6c8225333735fe5debf042b266494dc215929191
Marco <michaelis.marco@gmx.de>
2026-04-22 06:51:39 +0200

    Add files via upload

--- webpage/mainpage.py
+++ webpage/mainpage.py
@@ -9,6 +9,7 @@ data_mgmt = st.Page("dashboard/data_mgmt.py", title="Data Management", icon=":ma
 helps = st.Page("support/help.py", title="Help", icon=":material/help:")
 bugs = st.Page("support/bugs.py", title="Report a bug", icon=":material/bug_report:")
 features = st.Page("support/features.py", title="Feature request", icon=":material/notification_important:")
+about = st.Page("support/about.py", title="About project", icon=":material/check_circle:")
 
 search = st.Page("useful/search.py", title="Site Search", icon=":material/search:")
 insights = st.Page("useful/insight.py", title="Insights", icon=":material/history:")
@@ -17,7 +18,7 @@ pg = st.navigation(
     {
         "Dashboard & Data": [dashboard, packlinemx, data_mgmt],
         "Useful": [search, insights],
-        "Support": [helps, bugs, features],
+        "Support": [helps, bugs, features, about],
     }
 )
 pg.run()
