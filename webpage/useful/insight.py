c74d03b1fe3ad93d1ba22f64da98196fbb8fd951
Marco <michaelis.marco@gmx.de>
2026-04-22 20:40:10 +0200

    change of path

--- webpage/useful/insight.py
+++ webpage/useful/insight.py
@@ -12,8 +12,11 @@ st.markdown("<br>", unsafe_allow_html=True)
 st.markdown(
     "##### <span style='color:darkred; text-decoration:underline; font-weight:bold;'>Project Basis - Conveyor & Machine</span>",
     unsafe_allow_html=True)
-image_path = "images/TheMachine.png"
-image = Image.open(image_path)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+img_path = BASE_DIR / "images" / "TheMachine.png"
+
+image = Image.open(img_path)
 st.image(image, caption="", width=800)
 
 st.divider()
