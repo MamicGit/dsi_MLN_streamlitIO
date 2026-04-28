59098348fce84de0574710d86a1e6a2e74eab708
Marco <michaelis.marco@gmx.de>
2026-04-28 11:54:27 +0200

    Add files via upload

--- webpage/dashboard/PackingLineMatrix.py
+++ webpage/dashboard/PackingLineMatrix.py
@@ -3,13 +3,17 @@ import pandas as pd
 
 st.set_page_config(page_title="MLN | Matrix", layout="wide", initial_sidebar_state="expanded")
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # 4 lines Headers of page + dropdowns right side
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 st.markdown("<u>Dashboard & Data ▪ PL-Matrix</u>", unsafe_allow_html=True)
 st.markdown("# **Packaging Line Matrix**")
+st.markdown("#### **for Operation Manager**")
 st.write("Packaging Line Summary")
 
 st.markdown("<br>", unsafe_allow_html=True)
 
-# Daten erstellen
+# simulate station status, just for illustrative purposes. In production environment of a company a configuration for each slam is required
 df = pd.DataFrame({
     "SlamLine": [f"SLAM{str(i).zfill(2)}" for i in range(1, 9)],
     "Conveyor Speed Risk": ["🔴", "🟡", "🟢", "🔴", "🟢", "🟡", "🟡", "🟢"],
